ea220d98f78f811a1ff5a109d23178af6fc51c5b
Giridhar S <sgiridhar14@gmail.com>
2022-08-04 10:24:56 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
  instance_type = "t2.micro"
  count = 1
  subnet_id = "subnet-0d47c7cc17ec8f644"
- key = "giridhar_Key"
+ key_name = "giridhar_Key"
  tags = {
    Name = "Giridhar_ubuntu"
   }
